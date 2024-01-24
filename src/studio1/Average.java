2b353c83ec6a15113fdc098d3666016d132817bf
surface <surface@LAPTOP-RT0TU485.dhcp.wustl.edu>
2024-01-24 14:21:30 -0600

    Studio 1 unfinished

---
+++ src/studio1/Average.java
@@ -0,0 +1,21 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Yo,sup?");
+		System.out.println("The first of two integers to be averaged?");
+		
+		int n1 = in.nextInt();
+		
+		int n2 = in.nextInt();
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
